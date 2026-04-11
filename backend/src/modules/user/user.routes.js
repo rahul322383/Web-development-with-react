deaea345fd517eebda887e36089f23c8f33f0cc0
rahul322383 <rahulkurm666@gmail.com>
2026-04-11 22:56:01 +0530

    Refactor user.routes: update authorization for patch user and add route for getting users by department

--- backend/src/modules/user/user.routes.js
+++ backend/src/modules/user/user.routes.js
@@ -6,14 +6,16 @@ const userController = require('./user.controller');
 const { createUserSchema, updateUserSchema } = require('./user.validation');
 
 const router = express.Router();
-
+ 
 router.use(authenticate);
 
 router.get('/dashboard/summary', authorize('Employee', 'Manager', 'HR', 'Finance', 'Admin'), userController.getDashboardSummary);
 router.get('/', authorize('HR', 'Admin', 'Manager'), userController.listUsers);
 router.get('/:id', authorize('HR', 'Admin', 'Manager'), userController.getUserById);
 router.post('/', authorize('HR', 'Admin'), validate(createUserSchema), userController.createUser);
-router.patch('/:id', authorize('HR', 'Admin'), validate(updateUserSchema), userController.updateUser);
+router.patch('/:id', authorize('HR', 'Admin','Manager'), validate(updateUserSchema), userController.updateUser);
 router.delete('/:id', authorize('Admin'), userController.deleteUser);
+
+router.get('/department/:department', authorize('HR', 'Admin', 'Manager'), userController.getUsersByDepartment);
 
 module.exports = router;
