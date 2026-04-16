58937fc0bcafa0a090bd9a936c2dece6a0386210
rahul322383 <rahulkurm666@gmail.com>
2026-04-16 11:51:00 +0530

    refactor: remove commented-out sequelize configuration for cleaner code

--- backend/src/database/sequelize.js
+++ backend/src/database/sequelize.js
@@ -1,27 +1,3 @@
-// const { Sequelize } = require('sequelize');
-// const env = require('../config/env');
-// const logger = require('../config/logger');
-
-// const sequelize = new Sequelize(env.DB_NAME, env.DB_USER, env.DB_PASSWORD, {
-//   host: env.DB_HOST,
-//   port: env.DB_PORT,
-//   dialect: 'mysql',
-//   logging: (sql) => logger.debug({ sql }, 'sql-query'),
-//   pool: {
-//     max: env.DB_POOL_MAX,
-//     min: env.DB_POOL_MIN,
-//     acquire: 30000,
-//     idle: 10000
-//   },
-//   define: {
-//     underscored: true,
-//     timestamps: true,
-//     paranoid: true
-//   }
-// });
-
-// module.exports = sequelize;
-
 
 const { Sequelize } = require('sequelize');
 const env = require('../config/env');
