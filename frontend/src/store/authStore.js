380ee185361c8fb9a3433b7dbb82ecfb74aa4ecc
rahul322383 <rahulkurm666@gmail.com>
2026-04-18 15:37:57 +0530

    refactor: comment out unused authentication store code

--- frontend/src/store/authStore.js
+++ frontend/src/store/authStore.js
@@ -1,36 +1,36 @@
-import { create } from 'zustand';
+// import { create } from 'zustand';
 
-const useAuthStore = create((set) => ({
-  user: null,
-  accessToken: localStorage.getItem('accessToken') || null,
-  refreshToken: localStorage.getItem('refreshToken') || null,
-  isAuthenticated: !!localStorage.getItem('accessToken'),
+// const useAuthStore = create((set) => ({
+//   user: null,
+//   accessToken: localStorage.getItem('accessToken') || null,
+//   refreshToken: localStorage.getItem('refreshToken') || null,
+//   isAuthenticated: !!localStorage.getItem('accessToken'),
 
-  setAuth: (user, accessToken, refreshToken) => {
-    localStorage.setItem('accessToken', accessToken);
-    if (refreshToken) {
-      localStorage.setItem('refreshToken', refreshToken);
-    }
-    set({
-      user,
-      accessToken,
-      refreshToken,
-      isAuthenticated: true
-    });
-  },
+//   setAuth: (user, accessToken, refreshToken) => {
+//     localStorage.setItem('accessToken', accessToken);
+//     if (refreshToken) {
+//       localStorage.setItem('refreshToken', refreshToken);
+//     }
+//     set({
+//       user,
+//       accessToken,
+//       refreshToken,
+//       isAuthenticated: true
+//     });
+//   },
 
-  setUser: (user) => set({ user }),
+//   setUser: (user) => set({ user }),
 
-  logout: () => {
-    localStorage.removeItem('accessToken');
-    localStorage.removeItem('refreshToken');
-    set({
-      user: null,
-      accessToken: null,
-      refreshToken: null,
-      isAuthenticated: false
-    });
-  }
-}));
+//   logout: () => {
+//     localStorage.removeItem('accessToken');
+//     localStorage.removeItem('refreshToken');
+//     set({
+//       user: null,
+//       accessToken: null,
+//       refreshToken: null,
+//       isAuthenticated: false
+//     });
+//   }
+// }));
 
-export default useAuthStore;
+// export default useAuthStore;
