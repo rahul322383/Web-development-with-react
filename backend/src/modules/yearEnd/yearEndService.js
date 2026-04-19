bd23c7ea77d6d0c633b8a11f1b65111157ad0f21
rahul322383 <rahulkurm666@gmail.com>
2026-04-19 10:20:49 +0530

    feat: refactor year-end summary processing to use concurrency with p-limit

--- backend/src/modules/yearEnd/yearEndService.js
+++ backend/src/modules/yearEnd/yearEndService.js
@@ -1,38 +1,56 @@
-const sequelize = require('../../database/sequelize');              
-const yearEndRepository = require('./yearEndRepository');           
-const { clearCacheKeys } = require('../../utils/cache');   
+const sequelize = require('../../database/sequelize');
+const yearEndRepository = require('./yearEndRepository');
+const { clearCacheKeys } = require('../../utils/cache');
+const pLimit = require('p-limit');
 
+const processEmployee = async (employee, year, transaction) => {
+  try {
+    const [totalSalaryPaid, totalLeavesTaken, totalExpensesClaimed] = await Promise.all([
+      yearEndRepository.getSalaryTotal(employee.id, year, transaction),
+      yearEndRepository.getApprovedLeavesCount(employee.id, year, transaction),
+      yearEndRepository.getExpenseTotal(employee.id, year, transaction)
+    ]);
 
-const generateYearSummary = async (year) =>
-  sequelize.transaction(async (transaction) => {
-    const employees = await yearEndRepository.listActiveEmployees();
-    const summaries = [];
+    const summary = await yearEndRepository.upsertYearSummary(
+      {
+        employeeId: employee.id,
+        year,
+        totalSalaryPaid,
+        totalLeavesTaken,
+        totalExpensesClaimed,
+        isFinalized: true
+      },
+      transaction
+    );
 
-    for (const employee of employees) {
-      const [totalSalaryPaid, totalLeavesTaken, totalExpensesClaimed] = await Promise.all([
-        yearEndRepository.getSalaryTotal(employee.id, year),
-        yearEndRepository.getApprovedLeavesCount(employee.id, year),
-        yearEndRepository.getExpenseTotal(employee.id, year)
-      ]);
+    return { summary, cacheKey: `dashboard_summary:${employee.id}:${year}` };
+  } catch (err) {
+    throw new Error(`Year summary failed for employee ${employee.id}`, { cause: err });
+  }
+};
 
-      const summary = await yearEndRepository.upsertYearSummary(
-        {
-          employeeId: employee.id,
-          year,
-          totalSalaryPaid,
-          totalLeavesTaken,
-          totalExpensesClaimed,
-          isFinalized: true
-        },
-        transaction
-      );
+const generateYearSummary = async (year) => {
+  const cacheKeys = [];
+  const limit = pLimit(10);
 
-      await clearCacheKeys([`dashboard_summary:${employee.id}:${year}`]);
-      summaries.push(summary);
-    }
+  const summaries = await sequelize.transaction(async (transaction) => {
+    const employees = await yearEndRepository.listActiveEmployees({ transaction });
 
-    return summaries;
+    const results = await Promise.all(
+      employees.map((employee) =>
+        limit(() => processEmployee(employee, year, transaction))
+      )
+    );
+
+    results.forEach(({ cacheKey }) => cacheKeys.push(cacheKey));
+
+    return results.map(({ summary }) => summary);
   });
+
+  await clearCacheKeys(cacheKeys);
+
+  return summaries;
+};
 
 const getYearSummaries = async (year) => yearEndRepository.listYearSummaries(year);
 
