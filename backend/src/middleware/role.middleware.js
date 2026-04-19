1a3cc87625544ee540c3c5aa5bbdb371df2c7f0d
rahul322383 <rahulkurm666@gmail.com>
2026-04-19 22:18:41 +0530

    feat: remove unnecessary console log from role middleware

--- backend/src/middleware/role.middleware.js
+++ backend/src/middleware/role.middleware.js
@@ -6,7 +6,6 @@ const restrictTo = (...roles) => {
         message: 'Unauthorized: user not found'
       });
     }
-    console.log("role middle", req.user || req.user.role )
 
     if (!roles.includes(req.user.role)) {
       return res.status(403).json({
