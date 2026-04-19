3e7a158c64a0de0964872a18032d29566f2aa097
rahul322383 <rahulkurm666@gmail.com>
2026-04-19 16:46:23 +0530

    feat: add logging for user role in role middleware

--- backend/src/middleware/role.middleware.js
+++ backend/src/middleware/role.middleware.js
@@ -6,6 +6,7 @@ const restrictTo = (...roles) => {
         message: 'Unauthorized: user not found'
       });
     }
+    console.log("role middle", req.user || req.user.role )
 
     if (!roles.includes(req.user.role)) {
       return res.status(403).json({
