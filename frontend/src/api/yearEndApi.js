3c4751ca88b5aa20a8da311d7586e9a0f151a6a5
rahul322383 <rahulkurm666@gmail.com>
2026-04-25 09:02:39 +0530

    refactor: remove commented-out code in yearEndApi.js for cleaner implementation

--- frontend/src/api/yearEndApi.js
+++ frontend/src/api/yearEndApi.js
@@ -1,28 +1,3 @@
-// import axiosInstance from './axios';
-
-// export const yearEndApi = {
-//   // 🔹 Get all year-end summaries
-//   getSummaries: async () => {
-//     try {
-//       const res = await axiosInstance.get('/year-end');
-//       return res.data.data || res.data;
-//     } catch (error) {
-//       throw error.response?.data || error.message;
-//     }
-//   },
-
-//   // 🔹 Generate year-end summary
-//   generateSummary: async (payload) => {
-//     try {
-//       const res = await axiosInstance.post('/year-end/generate', payload);
-//       return res.data;
-//     } catch (error) {
-//       throw error.response?.data || error.message;
-//     }
-//   }
-// };
-
-// api/yearEnd.js
 import axiosInstance from './axios';
 
 export const yearEndApi = {
