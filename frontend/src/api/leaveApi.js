70afc55b832a0801abf1226593b50d046a312694
rahul322383 <rahulkurm666@gmail.com>
2026-04-30 11:39:08 +0530

    refactor: reorganize leaveApi structure and fix cancelLeave endpoint

--- frontend/src/api/leaveApi.js
+++ frontend/src/api/leaveApi.js
@@ -1,5 +1,73 @@
-import axiosInstance from "./axios";
+// import axiosInstance from "./axios";
 
+
+// export const leaveApi = {
+//   applyLeave: async (data, options = {}) => {
+//     const res = await axiosInstance.post("/leaves", data, {
+//       signal: options.signal
+//     });
+//     return res.data;
+//   },
+
+//   getMyLeaves: async (options = {}) => {
+//     const res = await axiosInstance.get("/leaves/my", {
+//       signal: options.signal
+//     });
+//     return res.data;
+//   },
+
+//   getLeaveBalance: async (options = {}) => {
+//     const res = await axiosInstance.get("/leaves/balance", {
+//       signal: options.signal
+//     });
+//     return res.data;
+//   },
+
+//   getPendingLeaves: async (options = {}) => {
+//     const res = await axiosInstance.get("/leaves/pending-manager", {
+//       signal: options.signal
+//     });
+//     return res.data;
+//   },
+
+//   reviewLeave: async (id, data, options = {}) => {
+//     const res = await axiosInstance.patch(`/leaves/${id}/review`, data, {
+//       signal: options.signal
+//     });
+//     return res.data;
+//   },
+
+//   getTeamLeaves: async (options = {}) => {
+//     const res = await axiosInstance.get("/leaves/team", {
+//       signal: options.signal
+//     });
+//     return res.data;
+//   },
+
+//   getLeaveStats: async (options = {}) => {
+//     const res = await axiosInstance.get("/leaves/stats", {
+//       signal: options.signal
+//     });
+//     return res.data;
+//   },
+
+//   cancelLeave: async (id, options = {}) => {
+//     const res = await axiosInstance.patch(`/leaves/${id}/review`, {}, {
+//       signal: options.signal
+//     });
+//     return res.data;
+//   },
+
+//   getLeaveById: async (id, options = {}) => {
+//     const res = await axiosInstance.get(`/leaves/${id}`, {
+//       signal: options.signal
+//     });
+//     return res.data;
+//   }
+
+
+// };
+import axiosInstance from "./axios";
 
 export const leaveApi = {
   applyLeave: async (data, options = {}) => {
@@ -51,9 +119,10 @@ export const leaveApi = {
     return res.data;
   },
 
+  // 🔧 FIX 3: Proper cancel endpoint (must exist on backend)
   cancelLeave: async (id, options = {}) => {
-    const res = await axiosInstance.patch(`/leaves/${id}/review`, {}, {
-      signal: options.signal
+    const res = await axiosInstance.patch(`/leaves/${id}/cancel`, {}, {
+      signal: options.signal,
     });
     return res.data;
   },
@@ -64,6 +133,4 @@ export const leaveApi = {
     });
     return res.data;
   }
-
-
 };
