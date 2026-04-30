96de5ddfc0cea4c4143fb07d11b1391778af9487
rahul322383 <rahulkurm666@gmail.com>
2026-04-30 11:37:01 +0530

    feat: add OPENAI_API_KEY to environment schema with default value

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -36,6 +36,7 @@ const envSchema = Joi.object({
   REDIS_PORT: Joi.number().required(),
   REDIS_PASSWORD: Joi.string().allow('').optional(),
   REDIS_DB: Joi.number().default(0),
+  OPENAI_API_KEY: Joi.number().default(0),
 
   CORS_ORIGIN: Joi.string().required(),
 
