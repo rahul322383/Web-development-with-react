ee28a165ceeea5cd8469cbf806106bce2eb0217a
rahul322383 <rahulkurm666@gmail.com>
2026-05-02 09:53:12 +0530

    refactor: fix formatting in Payroll model by removing unnecessary whitespace

--- backend/src/models/payroll.model.js
+++ backend/src/models/payroll.model.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.BIGINT.UNSIGNED,
         allowNull: false
       },
+      
       month: {
         type: DataTypes.INTEGER.UNSIGNED,
         allowNull: false
